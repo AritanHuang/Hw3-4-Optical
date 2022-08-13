c609e04158f382c106203b76f167142294cc795b
Aritan <aritan840703@gmail.com>
2022-08-13 13:47:42 +0800

    Update 2022-08-13T05:47:40.059Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,11 @@
 "use strict";
 
-console.log('Hello!');
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
+  // mobile版header區塊漢堡選單
+  $('.show-menu').click(function (e) {
+    e.preventDefault();
+    $('body').toggleClass('open-menu');
+  });
 });
 "use strict";
 
